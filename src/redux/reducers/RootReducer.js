82be7f110644079410634366d173a705a0d5d932
Daniel Adesanya <dcadesanya@sidmach.com>
2022-03-01 10:38:06 +0100

    Created reducers

--- src/redux/reducers/RootReducer.js
+++ src/redux/reducers/RootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
 import ProfileReducer from "./ProfileReducer";
+import categoryReducer from "./CategoryReducer";
 
 
 const RootReducer = combineReducers({
   userAuth: AuthReducer,
   userDetails: ProfileReducer,
+  category: categoryReducer,
 });
 
 export default RootReducer;
