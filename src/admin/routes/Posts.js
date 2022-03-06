930bf1cba8d1203baef3b4afc4d7eb4b42f58741
Daniel Adesanya <dcadesanya@sidmach.com>
2022-03-06 23:50:10 +0100

    Add Categories button

--- src/admin/routes/Posts.js
+++ src/admin/routes/Posts.js
@@ -1,25 +1,59 @@
-import StatusCard from 'components/adminComponents/StatusCard';
-import TableCard from 'components/adminComponents/TableCard';
-import { PrimaryButton } from 'components/misc/Buttons'
-import tw from 'twin.macro'
+import React, { useState } from "react";
+import StatusCard from "components/adminComponents/StatusCard";
+import TableCard from "components/adminComponents/TableCard";
+import { PrimaryButton } from "components/misc/Buttons";
+import tw from "twin.macro";
+import Button from "@material-tailwind/react/Button";
+import Spinner from "components/misc/Spinner";
+import LargeModal from "components/modal/LargeModal";
+import Categories from "components/adminComponents/Categories";
+import Icon from "@material-tailwind/react/Icon";
 
 const CardsGroup = tw.div`bg-blue-500 pt-14 pb-28 px-3 md:px-8 h-auto`;
-const CardsContainer = tw.div`container mx-auto max-w-full`
-const CardsGrid = tw.div`grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4`
+const CardsContainer = tw.div`container mx-auto max-w-full`;
+const CardsGrid = tw.div`grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4`;
 
-const SecondGroup = tw.div`px-3 md:px-8 h-auto -mt-24`
-const SecondConatainer = tw.div`container mx-auto max-w-full`
-const SecondGrid = tw.div`grid grid-cols-1 px-4 mb-16`
+const CardHolder = tw.div`px-4 mb-10 col-span-1 flex justify-between`;
+const SecondGroup = tw.div`px-3 md:px-8 h-auto -mt-24`;
+const SecondConatainer = tw.div`container mx-auto max-w-full`;
+const SecondGrid = tw.div`grid grid-cols-1 px-4 mb-16`;
 
-const ButtonContainer = tw.div`relative w-full`
+const ButtonContainer = tw.div`relative w-full`;
 
 export default function Dashboard() {
-    return (
-        <>
-            <CardsGroup>
-                <CardsContainer>
-                    <CardsGrid>
-                        {/* <StatusCard
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <>
+      <CardsGroup>
+        <CardsContainer>
+          {/* <CardsGrid> */}
+          <CardHolder>
+            <Button
+              color="orange"
+              buttonType="outline"
+              size="sm"
+              rounded={true}
+              // block={false}
+              // iconOnly={false}
+              ripple="light"
+              onClick={() => setOpenModal(true)}
+            >
+              <Icon name="settings" />
+              Set Categories
+            </Button>
+            <Button
+              color="purple"
+              buttonType="filled"
+              size="lg"
+              // rounded={false}
+              // block={false}
+              // iconOnly={false}
+              ripple="light"
+            >
+              Create new Post <Icon name="add" />
+            </Button>
+          </CardHolder>
+          {/* <StatusCard
                             color="pink"
                             icon="trending_up"
                             title="Traffic"
@@ -29,7 +63,7 @@ export default function Dashboard() {
                             percentageColor="green"
                             date="Since last month"
                         /> */}
-                        <StatusCard
+          {/* <StatusCard
                             color="orange"
                             icon="groups"
                             title="New Users"
@@ -48,8 +82,8 @@ export default function Dashboard() {
                             percentageIcon="arrow_downward"
                             percentageColor="orange"
                             date="Since yesterday"
-                        />
-                        {/* <StatusCard
+                        /> */}
+          {/* <StatusCard
                             color="blue"
                             icon="poll"
                             title="Performance"
@@ -59,20 +93,20 @@ export default function Dashboard() {
                             percentageColor="green"
                             date="Since last month"
                         /> */}
-                        <ButtonContainer>
-                            <PrimaryButton class="absolute top-0 right-0 h-16 w-16">New Post</PrimaryButton>
-                        </ButtonContainer>
-                    </CardsGrid>
-                </CardsContainer>
-            </CardsGroup>
+          {/* </CardsGrid> */}
+        </CardsContainer>
+      </CardsGroup>
 
-            <SecondGroup>
-                <SecondConatainer>
-                    <SecondGrid>
-                        <TableCard />
-                    </SecondGrid>
-                </SecondConatainer>
-            </SecondGroup>
-        </>
-    );
+      <SecondGroup>
+        <SecondConatainer>
+          <SecondGrid>
+            <TableCard title="Posts" />
+          </SecondGrid>
+        </SecondConatainer>
+      </SecondGroup>
+      <LargeModal title="Categories" setOpenModal={setOpenModal} active={openModal}>
+        <Categories />
+      </LargeModal>
+    </>
+  );
 }
