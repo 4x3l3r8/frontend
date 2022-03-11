cc944f7df3d6a5cfd9fd551752e5ca307162be27
Daniel Adesanya <dcadesanya@sidmach.com>
2022-03-11 14:47:52 +0100

    Category API url correction

--- src/apis/Category.js
+++ src/apis/Category.js
@@ -5,19 +5,19 @@ const Category = {
         return axios.get('http://localhost:8000/api/categories?page=' + page);
     },
     add: (title) => {
-        return axios.post('/categories', { title }, { headers: { Authorization: 'Bearer ' + localStorage.getItem("user.api_token") } });
+        return axios.post('http://localhost:8000/api/categories', { title }, { headers: { Authorization: 'Bearer ' + localStorage.getItem("user-token") } });
     },
     showOne: (id) => {
-        return axios.get('/categories/' + id);
+        return axios.get('http://localhost:8000/api/categories/' + id, null, );
     },
     edit: (title, id) => {
-        return axios.put('/categories/' + id, { title }, { headers: { Authorization: 'Bearer ' + localStorage.getItem("user.api_token") } });
+        return axios.put('http://localhost:8000/api/categories/' + id, { title }, { headers: { Authorization: 'Bearer ' + localStorage.getItem("user-token") } });
     },
     remove: (id) => {
-        return axios.delete('/categories/' + id, { headers: { Authorization: 'Bearer ' + localStorage.getItem("user.api_token") } });
+        return axios.delete('http://localhost:8000/api/categories/' + id, { headers: { Authorization: 'Bearer ' + localStorage.getItem("user.api_token") } });
     },
     listAll: () => {          // used to populate dropdowns
-        return axios.get('/categories?all=1');
+        return axios.get('http://localhost:8000/api/categories?all=1');
     }
 };
 
